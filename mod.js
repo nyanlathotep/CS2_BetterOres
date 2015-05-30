fb96bb3623a469c1f210d27f4c0ec10eaf73351a
pseudobyte <pseudobyte@gmail.com>
2015-05-30 01:17:20 -0600

    added smalldusts to mod.js

--- mod.js
+++ mod.js
@@ -1,6 +1,8 @@
 mod.addBlock('ores.js', 'normal');
 mod.addBlock('ores2.js', 'normal');
 mod.addBlock('ores3.js', 'normal');
+
 mod.addItem('chunks.js', 'normal');
 mod.addItem('chunks2.js', 'normal');
 mod.addItem('chunks3.js', 'normal');
+mod.addItem('smallDusts.js', 'normal');
