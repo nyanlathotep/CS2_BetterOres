3b7807a02ad24b68e725a59050b13a0bbaa967aa
pseudobyte <pseudobyte@gmail.com>
2015-05-30 01:15:22 -0600

    added small dusts

---
+++ items/smallDusts.js
@@ -0,0 +1,27 @@
+name = "smallDust";
+creativeTab = "materials";
+maxStack = 64;
+
+addToCreative[0] = true;
+textureFile[0] = "dustCopperSmall.png";
+
+addToCreative[1] = true;
+textureFile[1] = "dustGoldSmall.png";
+
+addToCreative[2] = true;
+textureFile[2] = "dustIronSmall.png";
+
+addToCreative[3] = true;
+textureFile[3] = "dustLeadSmall.png";
+
+addToCreative[4] = true;
+textureFile[4] = "dustNickelSmall.png";
+
+addToCreative[5] = true;
+textureFile[5] = "dustPlatinumSmall.png";
+
+addToCreative[6] = true;
+textureFile[6] = "dustSilverSmall.png";
+
+addToCreative[7] = true;
+textureFile[7] = "dustTinSmall.png";
