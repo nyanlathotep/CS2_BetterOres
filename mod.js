27ab4614dcd0326237f75cc3facab42376cd4bd5
Madcat1030 <madcat1030@gmail.com>
2015-05-31 03:03:24 -0400

    ADDED MINI COAL RECIPES.  I AM SO SLOW

--- mod.js
+++ mod.js
@@ -12,3 +12,7 @@ mod.addFuel('BetterOres:chunk3:3', 3200);
 mod.addFuel('BetterOres:chunk3:4', 200);
 mod.addFuel('BetterOres:chunk3:5', 200);
 mod.addFuel('BetterOres:chunk3:6', 200);
+
+mod.addShapelessRecipe('BetterOres:chunk3:4 4', 'BetterOres:chunk3:2');
+mod.addShapelessRecipe('BetterOres:chunk3:5 16', 'BetterOres:chunk3:3');
+mod.addShapelessrecipe('BetterOres:chunk3:6 16', 'Railcraft:fuel.coke');
