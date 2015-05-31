51214cff1bd07109af9caec748b0165cc0444038
pseudobyte <pseudobyte@gmail.com>
2015-05-31 00:26:19 -0600

    added coals, fixed some filenames

--- items/chunks3.js
+++ items/chunks3.js
@@ -7,3 +7,18 @@ textureFile[0] = "chunkCassiteriteSmall.png";
 
 addToCreative[1] = true;
 textureFile[1] = "chunkCassiterite.png";
+
+addToCreative[2] = true;
+textureFile[2] = "coalLignite.png";
+
+addToCreative[3] = true;
+textureFile[3] = "coalAnthracite.png";
+
+addToCreative[4] = true;
+textureFile[4] = "coalLigniteSmall.png";
+
+addToCreative[5] = true;
+textureFile[5] = "coalAnthraciteSmall.png";
+
+addToCreative[6] = true;
+textureFile[6] = "coalCokeSmall.png";
