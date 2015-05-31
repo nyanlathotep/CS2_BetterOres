1bbbd67ccbbc09c3ebc828e191c6d32fa65d8849
Madcat1030 <madcat1030@gmail.com>
2015-05-31 02:53:24 -0400

    ADDED COAL AS FUELS OMG THAT TOOK ME WAY TOO LONG... :(

--- mod.js
+++ mod.js
@@ -6,3 +6,9 @@ mod.addItem('chunks.js', 'normal');
 mod.addItem('chunks2.js', 'normal');
 mod.addItem('chunks3.js', 'normal');
 mod.addItem('smallDusts.js', 'normal');
+
+mod.addFuel('BetterOres:chunk3:2', 800);
+mod.addFuel('BetterOres:chunk3:3', 3200);
+mod.addFuel('BetterOres:chunk3:4', 200);
+mod.addFuel('BetterOres:chunk3:5', 200);
+mod.addFuel('BetterOres:chunk3:6', 200);
