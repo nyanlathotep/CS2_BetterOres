717938b8cd763f05ee280425c085c17cafb3f27a
pseudobyte <pseudobyte@gmail.com>
2015-06-01 01:50:23 -0600

    added lignite/anthracite blocks as fuels, added lang entries

--- scripts/fuel.js
+++ scripts/fuel.js
@@ -3,3 +3,6 @@ mod.addFuel('BetterOres:chunk3:3', 3200);
 mod.addFuel('BetterOres:chunk3:4', 200);
 mod.addFuel('BetterOres:chunk3:5', 200);
 mod.addFuel('BetterOres:chunk3:6', 200);
+
+mod.addFuel('BetterOres:ore3:6', 7200);
+mod.addFuel('BetterOres:ore3:7', 28800);
